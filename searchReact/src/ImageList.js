e2d27430de2c77e0516c23acb10221ad9064757c
Ahmet Metin Arslan <94191552+ahmetmetinarslan@users.noreply.github.com>
2023-09-20 16:42:52 +0300

    Update ImageList.js
    Kodlara yorum satırı eklendi.

--- searchReact/src/ImageList.js
+++ searchReact/src/ImageList.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ImageItem from './ImageItem'
 
-export default function ImageList({imagePlaceholders}) {
+// ImageList componentini tanımlar ve imagePlaceholders adında bir prop alır.
+export default function ImageList({ imagePlaceholders }) {
   return (
+    // Bir div elementi oluşturulur ve CSS sınıfı "ImageList" ile ilişkilendirilir.
     <div className='ImageList'>
-        {imagePlaceholders.map((image, index)=> {
-            return <ImageItem image={image} key={index}/>
-        })}
+      {imagePlaceholders.map((image, index) => {
+        // Her bir öğe için ImageItem bileşeni oluşturulur ve "image" prop'u ile iletilir.
+        return <ImageItem image={image} key={index} />;
+      })}
     </div>
-  )
+  );
 }
+
