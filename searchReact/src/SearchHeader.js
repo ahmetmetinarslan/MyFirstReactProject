2e1b88c718a2c98587ec045e4f86dc4acd3913d8
Ahmet Metin Arslan <94191552+ahmetmetinarslan@users.noreply.github.com>
2023-09-25 15:58:51 +0300

    Update SearchHeader.js
    comment Line added.

--- searchReact/src/SearchHeader.js
+++ searchReact/src/SearchHeader.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function SearchHeader({ Search }) {
+    // State kullanarak input değerini takip ediyoruz
     const [valueInput, setValueInput] = useState('');
+    
+    // Form gönderildiğinde çalışacak fonksiyon
     const handleFromSubmit = (event) => {
         event.preventDefault(); // formun sayfayı yenilemesi engellenidi.
-        Search(valueInput);
+        Search(valueInput); // search fonksiyonuna input değerini iletiyoruz.
     }
+     // Input değeri değiştiğinde çalışacak fonksiyon
     const handleChange = (event) => {
-        setValueInput(event.target.value);
+        setValueInput(event.target.value); // Input değerini güncelliyoruz.
     }
+    //return edilen html yapısı
   return (
     <div className='searchDiv'>
         <form className='searchForm' onSubmit={handleFromSubmit}>
